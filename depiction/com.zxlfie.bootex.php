f5fa11467f4a73f3cea28e911504b681fe1a7e6a
Zxlfie <zxlfie@gmail.com>
2016-09-23 16:47:38 +0000

    Pls

---
+++ depiction/com.zxlfie.bootex.php
@@ -0,0 +1,14 @@
+<?php
+	$isUDIDProtected=TRUE;
+	$compatibleVersions=array("8.1.1","8.1","8.0");
+    $name="BootEx";
+    $description=array(
+"BootEx is basically a bunch of respring logos put into one big tweak. ");
+
+    $changelog=array(
+"<b>1.0.5:</b>",
+" - Initial release without bugs.");
+
+    $devText="@Zxlfies";
+    $devLink="https://twitter.com/Zxlfies";
+?>
