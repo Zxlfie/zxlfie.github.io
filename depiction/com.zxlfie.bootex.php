736b1925a3b70de040f6bcbd5a1f3d99c54f0d1e
Zxlfie <zxlfie@gmail.com>
2016-09-23 17:11:41 +0000

    Changing stuff

--- depiction/com.zxlfie.bootex.php
+++ depiction/com.zxlfie.bootex.php
@@ -1,6 +1,5 @@
 <?php
 	$isUDIDProtected=TRUE;
-	$compatibleVersions=array("8.1.1","8.1","8.0");
     $name="BootEx";
     $description=array(
 "BootEx is basically a bunch of respring logos put into one big tweak. ");
